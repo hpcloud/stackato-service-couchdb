bc38c0c883d91e2a88fb89295a7aa23244fec4a1
Steven Hsu <admin@stevenhsu.ca>
2013-10-11 21:38:44 -0700

    Minor corrections.

--- scripts/install_couchdb.sh
+++ scripts/install_couchdb.sh
@@ -4,17 +4,17 @@
 # apt-get update
 
 # get couchdb
-wget https://dl.dropboxusercontent.com/u/13515458/couchdb.tar.gz
+wget https://dl.dropboxusercontent.com/u/13515458/couchdb-1.4.tar.gz
 
 # untar and put it in /opt/
 tar -zxf couchdb.tar.gz
 mv build-couchdb /opt/
-
-# set correct permissions
-chown --recursive stackato:stackato /opt/build-couchdb
 
 #setup path
 . /opt/build-couchdb/build/env.sh
 
 # clean up source
 rm couchdb.tar.gz
+
+# set correct permissions
+# chown --recursive stackato:stackato /opt/build-couchdb
