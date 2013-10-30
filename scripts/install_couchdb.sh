10c3f20b9604e121e5f66b393ef8ceb3f8606f02
Steven Hsu <admin@stevenhsu.ca>
2013-10-29 20:29:39 -0700

    Fixed script errors.

--- scripts/install_couchdb.sh
+++ scripts/install_couchdb.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # ===== install the libraries which couchdb needs ===== #
+sudo apt-get update
 sudo apt-get install erlang libicu42 libicu-dev libcurl4-openssl-dev
 
 
 # ===== install Spidermonkey 1.9.2 from PPA ===== #
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 74EE6429
+sudo apt-key adv --keyserver http://keyserver.ubuntu.com --recv-keys 74EE6429
 sudo bash -c 'echo "deb http://ppa.launchpad.net/commonjs/ppa/ubuntu karmic main" >> /etc/apt/sources.list.d/commonjs.list'
 sudo apt-get update
 sudo apt-get install libmozjs-1.9.2 libmozjs-1.9.2-dev
@@ -13,10 +14,10 @@ sudo ln -s /usr/lib/libmozjs-1.9.2.so /usr/lib/libmozjs.so
 
 
 # ===== download and install couchdb ===== #
-wget http://apache.mirror.rafal.ca/couchdb/source/1.4.0/apache-couchdb-1.4.0.tar.gz 
-tar zxvf apache-couchdb-1.4.0.tar.gz
-rm apache-couchdb-1.4.0.tar.gz
-cd apache-couchdb-1.4.0
+wget https://github.com/cloudant/bigcouch/archive/bigcouch-0.4.2b.zip
+unzip bigcouch-0.4.2b
+rm bigcouch-0.4.2b.zip
+cd bigcouch-0.4.2b
 ./configure
 make
 sudo make install
