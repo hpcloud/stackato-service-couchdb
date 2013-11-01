a5d0888678ab0c0045cbb5924ad32dabf9a12a5d
Steven Hsu <admin@stevenhsu.ca>
2013-11-01 09:57:27 -0700

    Prompt for cc url and misc setup script fixes.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -19,15 +19,20 @@ cat /s/vcap/services/couchdb/stackato-conf/roles-snippet.yml >> /s/etc/kato/role
 # Restart supervisord
 start-supervisord
 
+# Add the authentication token to the cloud controller
+SERVICE_TOKEN=`date +%s | sha256sum | base64 | head -c 10`
+kato config set cloud_controller builtin_services/couchdb/token "$SERVICE_TOKEN"
+echo "token: $SERVICE_TOKEN" >> /s/vcap/services/couchdb/config/couchdb_gateway.yml
+
+# Set cc url
+echo "Please enter cloud controller api url (api.stackato.local): "
+read CCURL
+echo "cloud_controller_uri: $CCURL" >> /s/vcap/services/couchdb/config/couchdb_gateway.yml
+
 # set kato config
 cat /s/vcap/services/couchdb/config/couchdb_gateway.yml | kato config set couchdb_gateway / --yaml
 cat /s/vcap/services/couchdb/config/couchdb_node.yml | kato config set couchdb_node / --yaml
 
-# Add the authentication token to the cloud controller
-SERVICE_TOKEN=`date +%s | sha256sum | base64 | head -c 10`
-
-kato config set cloud_controller builtin_services/couchdb/token "$SERVICE_TOKEN"
-echo "token: $SERVICE_TOKEN" >> /s/vcap/services/couchdb/config/couchdb_gateway.yml
 # Add the role and restart kato
 kato role add couchdb
 kato start
@@ -36,7 +41,7 @@ kato start
 COUCHDB_PASSWORD=`date +%s | sha256sum | base64 | head -c 16`
 COUCHDB_HOST=localhost:5984
 
-curl -X PUT http://localhost:5984/_config/admins/admin -d'"$COUCHDB_PASSWORD"'
+curl -X PUT http://localhost:5984/_config/admins/admin -d'"'$COUCHDB_PASSWORD'"'
 echo "couchdb_password: $COUCHDB_PASSWORD" >> /s/vcap/services/couchdb/config/couchdb_node.yml
 echo "couchdb_host: $COUCHDB_HOST" >> /s/vcap/services/couchdb/config/couchdb_node.yml
 
