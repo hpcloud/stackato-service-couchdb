a181692a59400402280b9ac07b6dc29a788231f4
Steven Hsu <admin@stevenhsu.ca>
2013-10-31 23:05:56 -0700

    Forgot to add force yes.

--- scripts/install_bigcouch.sh
+++ scripts/install_bigcouch.sh
@@ -4,7 +4,7 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 74EE6429
 echo "deb http://packages.cloudant.com/ubuntu `lsb_release -cs` main" | sudo tee /etc/apt/sources.list.d/cloudant.list
 sudo apt-get update
-sudo apt-get install bigcouch -y
+sudo apt-get install bigcouch -y --force-yes
 
 sudo cp -f bigcouch_default_config.args /opt/bigcouch/etc/vm.args
 
