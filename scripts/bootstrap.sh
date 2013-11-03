efba97178743784d1d71151ae38e39da8d81bbc2
Steven Hsu <admin@stevenhsu.ca>
2013-11-03 00:02:22 -0700

    Fixed the command ordering again.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -19,30 +19,31 @@ cat /s/vcap/services/couchdb/stackato-conf/roles-snippet.yml >> /s/etc/kato/role
 # Restart supervisord
 start-supervisord
 
-# Add the authentication token to the cloud controller
+# Set cc url in config
+echo "Please enter cloud controller api url (api.stackato.local): "
+read CCURL
+echo "cloud_controller_uri: $CCURL" >> /s/vcap/services/couchdb/config/couchdb_gateway.yml
+
+# Add the authentication token to the cloud controller and set it in config
 SERVICE_TOKEN=`date +%s | sha256sum | base64 | head -c 10`
 kato config set cloud_controller builtin_services/couchdb/token "$SERVICE_TOKEN"
 echo "token: $SERVICE_TOKEN" >> /s/vcap/services/couchdb/config/couchdb_gateway.yml
 
-# Set cc url
-echo "Please enter cloud controller api url (api.stackato.local): "
-read CCURL
-echo "cloud_controller_uri: $CCURL" >> /s/vcap/services/couchdb/config/couchdb_gateway.yml
+# Set couchdb_password / couchdb_hostname in config
+COUCHDB_PASSWORD=`date +%s | sha256sum | base64 | head -c 16`
+COUCHDB_HOSTNAME=localhost:5984
+echo "couchdb_password: $COUCHDB_PASSWORD" >> /s/vcap/services/couchdb/config/couchdb_node.yml
+echo "couchdb_hostname: $COUCHDB_HOSTNAME" >> /s/vcap/services/couchdb/config/couchdb_node.yml
 
 # set kato config
 cat /s/vcap/services/couchdb/config/couchdb_gateway.yml | kato config set couchdb_gateway / --yaml
 cat /s/vcap/services/couchdb/config/couchdb_node.yml | kato config set couchdb_node / --yaml
 
+# setup first couchdb admin
+curl -X PUT http://$COUCHDB_HOSTNAME/_config/admins/admin -d'"'$COUCHDB_PASSWORD'"'
+
 # Add the role and restart kato
 kato role add couchdb
 kato start
-
-# setup first couchdb user
-COUCHDB_PASSWORD=`date +%s | sha256sum | base64 | head -c 16`
-COUCHDB_HOST=localhost:5984
-
-curl -X PUT http://localhost:5984/_config/admins/admin -d'"'$COUCHDB_PASSWORD'"'
-echo "couchdb_password: $COUCHDB_PASSWORD" >> /s/vcap/services/couchdb/config/couchdb_node.yml
-echo "couchdb_host: $COUCHDB_HOST" >> /s/vcap/services/couchdb/config/couchdb_node.yml
 
 echo Success!
